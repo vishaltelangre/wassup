5c146dca4331490bb66f4e33080ca8434fbffa00
Vishal Telangre <the@vishaltelangre.com>
2019-12-18 17:03:18 +0530

    Refactor dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -4,20 +4,19 @@ const showClassName = "show";
 
 document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener("click", ({target}) => {
+    // Close all expanded dropdowns
+    const dropdowns = document.getElementsByClassName(contentClassName);
+    for (let dropdown of dropdowns) {
+      if (dropdown.classList.contains(showClassName)) {
+        dropdown.classList.remove(showClassName);
+      }
+    }
     // When user clicks on a dropdown trigger,
     // toggle show/hide the resembled dropdown contents
     const closestTriggerTarget = target && target.closest(`.${triggerClassName}`);
     if (closestTriggerTarget) {
       const content = closestTriggerTarget.parentNode.querySelector(`.${contentClassName}`);
-      content.classList.toggle("show");
-    // Close the dropdown if user clicks outside of the dropdown
-    } else {
-      const dropdowns = document.getElementsByClassName(contentClassName);
-      for (let dropdown of dropdowns) {
-        if (dropdown.classList.contains(showClassName)) {
-          dropdown.classList.remove(showClassName);
-        }
-      }
+      content.classList.add("show");
     }
   }, false);
 });
