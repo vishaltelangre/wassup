a5dcf9bad41fce87a3854cd3a189b67ba5825323
Vishal Telangre <the@vishaltelangre.com>
2019-12-29 15:46:30 +0530

    Prevent body scroll when modal is shown

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,17 +2,20 @@ const modalSelector = "[data-behavior=modal]";
 const modalContentSelector = "[data-behavior=modal-content]";
 const modalCloseSelector = "[data-behavior=modal-close]";
 const modalShowClassName = "show";
+const noScrollClassName = "noscroll";
 
 const showModal = contents => {
   const modal = document.querySelector(modalSelector);
 
   modal.querySelector(modalContentSelector).innerHTML = contents;
   modal.classList.add(modalShowClassName);
+  document.body.classList.add(noScrollClassName);
 }
 
 const closeModal = () => {
   const modal = document.querySelector(modalSelector);
   modal.classList.remove(modalShowClassName);
+  document.body.classList.remove(noScrollClassName);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
