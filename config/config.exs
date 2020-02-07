a978c53f4b2f7aa7e78aba8648a74a1984a18d19
Vishal Telangre <the@vishaltelangre.com>
2020-02-07 19:25:29 +0530

    Dockerize app

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 alias WassupApp.Utils
 
 config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -2,49 +2,18 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
-
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+import Config
 
 config :wassup_app, WassupApp.Repo,
   migration_primary_key: [name: :id, type: :binary_id],
   migration_timestamps: [type: :utc_datetime],
   ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
-
-live_view_signing_salt =
-  System.get_env("LIVE_VIEW_SIGNING_SALT") ||
-    raise """
-    environment variable LIVE_VIEW_SIGNING_SALT is missing.
-    You can generate one by calling: mix phx.gen.secret 32
-    """
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
 
 config :wassup_app, WassupAppWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: secret_key_base,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT", "4000"))],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   live_view: [
-    signing_salt: live_view_signing_salt
+    signing_salt: System.get_env("SECRET_KEY_BASE")
   ]
-
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :wassup_app, WassupAppWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
